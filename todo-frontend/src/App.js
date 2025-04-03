126b5f8cc2506bd9aaed50b52df77ffefdbe6ff6
EsterBenedikt <ester.benedikt@develeap.com>
2025-04-03 13:16:49 +0300

    style

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import TodoList from './components/TodoList';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <TodoList />
     </div>
   );
